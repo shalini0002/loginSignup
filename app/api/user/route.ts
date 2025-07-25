3b722a0874aa087ad5865934c911366a82042d64
Shalini Sharma <shalinisharma@Mac.lan>
2025-07-25 15:20:11 +0530

    signup authentication

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,8 +1,15 @@
 import { log } from "console";
 import { NextRequest } from "next/server";
-
+import { PrismaClient } from "../../generated/prisma";
+const client = new PrismaClient();
 export async function POST(req:NextRequest){
     const body = await req.json();
+    await client.user.create({
+        data: {
+            username: body.username,
+            password: body.password
+        }
+    })
     console.log(body);
     
 return Response.json({
